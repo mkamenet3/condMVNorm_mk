5295d05f4e71946ee3d9eada5b2df3ea8965ed2e
Maria Kamenetsky <maria.kamenetsky@gmail.com>
2024-06-20 16:49:18 -0400

    Update condMVNorm.r
    trying something

--- R/condMVNorm.r
+++ R/condMVNorm.r
@@ -21,6 +21,11 @@ D <- sigma[given.ind, given.ind]
 CDinv <- C %*% zapsmall(solve(D))
 cMu <- c(mean[dependent.ind] + CDinv %*% (X.given - mean[given.ind]))
 cVar <- B - CDinv %*% t(C)
+if(any(diag(cVar) < 0)){
+	CDinv <- C %*% solve(D)
+	cMu <- c(mean[dependent.ind] + CDinv %*% (X.given - mean[given.ind]))
+	cVar <- B - CDinv %*% t(C)
+}
 if(!isTRUE(all.equal(cVar, t(cVar))) || any(diag(cVar) < 0)){
       browser()
 }	
